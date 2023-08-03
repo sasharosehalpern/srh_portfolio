72e623a41c0a6af8a95edd45ee7c8b28f031f3e3
sasharosehalpern <sashahalpern626@gmail.com>
2023-08-03 13:56:09 -0500

    fixing formatting on dashboard site deployment

--- docs/app.js
+++ docs/app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/shared/QXRYXH7MM?:display_count=n&:origi
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1000,
-    width: 1200,
+    height: 800,
+    width: 1400,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
